83ba37b5c71988a37112e7a4b7408336b87f8883
mbearz <mbearz@gmail.com>
2016-07-31 20:31:09 +0300

    front end

--- index.js
+++ index.js
@@ -4,28 +4,29 @@
 *   Server router for Node.JS handler.
 * 
 **/
-var express      = require('express');
-var Promise      = require("bluebird");
-var express      = require('express');
-var path         = require('path');
-var favicon      = require('serve-favicon');
-var logger       = require('morgan');
+var express = require('express');
+var Promise = require("bluebird");
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var app          = express();
-var http         = require('http');
-var router       = express.Router();
-var routes       =require('./routes.js').routes;
+var bodyParser = require('body-parser');
+var app = express();
+var http = require('http');
+ 
+var router = express.Router();
+var routes = require('./routes.js').routes;
 //var mongoose    = require('mongoose');
 //var sequelize = require('sequelize');
 var jwt = require('jsonwebtoken');
-var knex= require('knex');
+var knex = require('knex');
 var common = require('./common.js');
 var ItembasketObject = require('./basket.js');
 var settings = require('./settings.js');
-
-global.Settings=settings;
-global.ItembasketObject=ItembasketObject;
+var mustacheExpress = require('mustache-express');
+global.Settings = settings;
+global.ItembasketObject = ItembasketObject;
 common();
 
 var knex = require('./knex.js');
@@ -38,13 +39,18 @@ var objects = require('./objects.js');
 global.Objects = objects;
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+app.engine('html', mustacheExpress());          // register file extension mustache
+app.set('view engine', 'html');                 // register file extension for partials
+app.set('views', __dirname + '/html');
+app.use('/web', express.static(__dirname + '/web'));// set static folder
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ 
-  extended: false 
+app.use(bodyParser.urlencoded({
+  extended: false
 }));
 
 app.use(cookieParser());
@@ -53,39 +59,48 @@ app.use(express.static(path.join(__dirname, 'public')));
 /**
 * Routing controllers sets.
 **/
+app.get('/', function (req, res) {
+    res.render('master', {
+        homepage: {
+            title: 'random hp',
+            items: [{_id:1,title:"mm" }, { _id: 2, title: "vvv" }]
+        }
+    });
+});
+
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Resurce Not Found');
   err.status = 404;
   next(err);
 });
 
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);  
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
     res.json({
-  	     "Error": err.message,
-      "status":err.status     
-  });
-   /* res.render('error', {
-      message: err.message,
-      error: err
-    });*/
+      "Error": err.message,
+      "status": err.status
+    });
+    /* res.render('error', {
+       message: err.message,
+       error: err
+     });*/
   });
 }
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
-    res.json({
-  	     "Error": err.message,
-      "status":err.status     
+  res.json({
+    "Error": err.message,
+    "status": err.status
   });
-   /* res.render('error', {
-      message: err.message,
-      error: err
-    });*/
+  /* res.render('error', {
+     message: err.message,
+     error: err
+   });*/
 });
 
 
--- routes.js
+++ routes.js
@@ -5,37 +5,37 @@ var jwt = require('jsonwebtoken');
 router.post("/api/external/login", require("./api/external/auth/postLogin.js").do);
 router.post("/api/external/register", require("./api/external/auth/register.js").do);
 // all routes below need to be authenticated 
-router.use(function (req, res, next) {
-  // check header or url parameters or post parameters for token
-  var token = req.body.token || req.query.token || req.headers['access-token'];
-  // decode token
-  if (token) {
-    // verifies secret and checks exp
-    jwt.verify(token, global.Settings.secret, function (err, decoded) {
-      if (err) 
-      {
-        return res.status(403).send({
-          success: false,
-          message: 'Failed to authenticate token.'
-        });
-      }
-      else {
-        // if everything is good, save to request for use in other routes
-        req.decoded = decoded;
-        next();
-      }
-    });
-  }
-  else {
-    // if there is no token
-    // return an error
-    return res.status(403).send({
-      success: false,
-      message: 'No token provided.'
-    });
+// router.use(function (req, res, next) {
+//   // check header or url parameters or post parameters for token
+//   var token = req.body.token || req.query.token || req.headers['access-token'];
+//   // decode token
+//   if (token) {
+//     // verifies secret and checks exp
+//     jwt.verify(token, global.Settings.secret, function (err, decoded) {
+//       if (err) 
+//       {
+//         return res.status(403).send({
+//           success: false,
+//           message: 'Failed to authenticate token.'
+//         });
+//       }
+//       else {
+//         // if everything is good, save to request for use in other routes
+//         req.decoded = decoded;
+//         next();
+//       }
+//     });
+//   }
+//   else {
+//     // if there is no token
+//     // return an error
+//     return res.status(403).send({
+//       success: false,
+//       message: 'No token provided.'
+//     });
 
-  }
-});
+//   }
+// });
 
 router.get("/api/external/basket", require("./api/open/findBasket.js").do);
 
@@ -61,6 +61,8 @@ router.post("/api/external/basket", require("./api/open/findBasket.js").do);
 // router.put("/api/external/branches/:id", require("./api/external/branches/putBranches.js").do);
 // router.put("/api/external/products/:id", require("./api/external/products/putProduct.js").do);
 
-
+router.get('/', function (req, res) {
+  res.render('index.html', { title: 'Hey', message: 'Hello there!'});
+});
 
 module.exports.routes = router;
